8c99c8e378d0691ebbd7169a643e23c50edc02a6
princessedeb <deb.dardour@gmail.com>
2024-10-20 10:45:21 +0200

    Delete greetings directory

--- greetings/script.js
+++
@@ -1,9 +0,0 @@
-// Fonction pour obtenir la date en français
-function afficherDate() {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date().toLocaleDateString('fr-FR', options);
-    document.getElementById('date').textContent = date;
-}
-
-// Appelle la fonction à l'ouverture de la page
-window.onload = afficherDate;
